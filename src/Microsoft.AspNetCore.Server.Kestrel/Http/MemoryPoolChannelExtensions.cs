52c125607a8dce0fd6522f5135ef51b27a1a355f
David Fowler <davidfowl@gmail.com>
2016-06-05 09:24:16 +0200

    Added Copy operation between MemoryPoolChannels

--- src/Microsoft.AspNetCore.Server.Kestrel/Http/MemoryPoolChannelExtensions.cs
+++ src/Microsoft.AspNetCore.Server.Kestrel/Http/MemoryPoolChannelExtensions.cs
@@ -1,13 +1,59 @@
 // Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
+using System;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Server.Kestrel.Infrastructure;
 
 namespace Microsoft.AspNetCore.Server.Kestrel.Http
 {
     public static class MemoryPoolChannelExtensions
     {
+        public static async Task CopyToAsync(this MemoryPoolChannel input, MemoryPoolChannel output)
+        {
+            while (true)
+            {
+                await input;
+
+                var fin = input.RemoteIntakeFin;
+
+                var begin = input.BeginRead();
+                var end = input.End();
+
+                try
+                {
+                    var block = begin.Block;
+
+                    var writeEnd = output.BeginWrite();
+
+                    while (true)
+                    {
+                        var blockStart = block == begin.Block ? begin.Index : block.Data.Offset;
+                        var blockEnd = block == end.Block ? end.Index : block.Data.Offset + block.Data.Count;
+
+                        writeEnd.CopyFrom(block.Array, blockStart, blockEnd - blockStart);
+
+                        if (block == end.Block)
+                        {
+                            break;
+                        }
+
+                        block = block.Next;
+                    }
+
+                    await output.EndWrite(writeEnd);
+                }
+                finally
+                {
+                    input.EndRead(end);
+                }
+
+                if (fin)
+                {
+                    return;
+                }
+            }
+        }
+
         public static ValueTask<int> ReadAsync(this MemoryPoolChannel input, byte[] buffer, int offset, int count)
         {
             while (input.IsCompleted)
